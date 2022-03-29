c53226cf00603f0f3d0cf61872a2cb72aaa30b85
iqbalhabil87@gmail.com <iqbalhabil87@gmail.com>
2022-03-29 19:33:13 +0700

    Tugas 3

---
+++ main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3_pbo;
+/**
+ *
+ * @author iqbal
+ */
+public class main {
+        public static void main(String[] args) {
+        tugas3_pbo tgs = new tugas3_pbo();
+        tgs.setVisible(true);
+    }
+}
